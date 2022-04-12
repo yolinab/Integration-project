5ea9ee2ad63d9d9c9a4dba3cf7d3c9ad59933fb1
Yolina <yolinab@gmail.com>
2022-04-12 17:02:49 +0200

    Discovery stage: -gave the medium state a boolean to check if it's free -added thread.sleep() if the medium is busy, to avoid collisions and packet loss

--- client/Message.java
+++ client/Message.java
@@ -24,4 +24,16 @@ public class Message {
         return data;
     }
 
+    /**
+     * Removes the SYN flag from a discovery message and places the IP in the second byte
+     * @param receiverIP the IP of the responder
+     * @return the ACK message in response to discovery SYN message
+     */
+    //@requires this.getData().capacity() == 2;
+    public Message respondToSYN(byte receiverIP) {
+        ByteBuffer buffer = this.getData();
+        buffer.put(1, (byte) ((buffer.get(1) + receiverIP) - 64));//add the IP and remove SYN flag
+        return new Message(MessageType.DATA_SHORT, buffer);
+    }
+
 }
