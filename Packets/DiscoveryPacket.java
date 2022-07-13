dc9975b98b39d745dcbc6aa9bd36b5250c4902a9
Yolina Yordanova <y.yordanova@student.utwente.nl>
2022-07-13 18:03:27 +0200

    Updated DiscoveryPacket implementation

--- Packets/DiscoveryPacket.java
+++ Packets/DiscoveryPacket.java
@@ -8,47 +8,30 @@ import java.util.ArrayList;
 
 /**
  * Class for the first type of packet being sent by the nodes in the discovery phase.
+ *
  * Message type: DATA_SHORT
  */
+public class DiscoveryPacket{
 
-//TODO: protocol handling packet loss
-public class DiscoveryPacket implements Packet {
-
-    private int sourceIp;
+    private final int sourceIp;
     private ByteBuffer buffer;
 
     /*
-                 Source           Flag(SYN/ACK)   Receiver puts their IP here
-            |_|_||_|_||_|_||_|_|      |_|_|          |_|_||_|_||_|_|
+                   Source                Flag(SYN/ACK)
+            |_|_||_|_||_|_||_|_|      |_|_||_|_||_|_||_|_|
      */
 
-    //@requires sourceIp != 0;
-    //@ensures buffer.capacity() == 2;
+    /**
+     * Makes an initial SYN packet containing the IP of the creator
+     * and a set SYN flag.
+     */
+    //@ensures buffer.capacity() == 2 && buffer.get(1) == 64;
     public DiscoveryPacket(int sourceIP) {
         super();
         this.sourceIp = sourceIP;
         buffer = ByteBuffer.allocate(2);
-    }
-
-    /**
-     * Makes an initial SYN packet containing the IP of the creator,
-     * a set SYN flag and an empty placeholder for the receivers IP.
-     */
-    //@ensures buffer.get(1) == 64;
-    public void makeSYN() {
         buffer.put((byte) (sourceIp));      //00000000 for source IP
         buffer.put((byte) 64);              //01000000 for SYN
-    }
-
-    /**
-     * When a node has received an initial SYN packet, it sends back a response with its IP
-     * The ACK flag is automatically set
-     *
-     * @param ip the IP of the neighbour who sends the ACK
-     */
-    //@requires ip != 0;
-    public void respond(int ip) {
-        buffer.put(1, (byte) ((buffer.get(1) + ip) - 64));//add the IP and remove SYN flag
     }
 
     /**
@@ -60,22 +43,6 @@ public class DiscoveryPacket implements Packet {
     public ByteBuffer getByteBuffer() {
         return buffer;
     }
-
-    /**
-     * Checks if the ACK flag is set, i.e. this is a response packet.
-     *
-     * @return true is this packet is a response to a discovery packet
-     */
-    //@requires buffer != null;
-    public boolean isACKed() {
-        return buffer.get(1) < 64;
-    }
-
-
-    /**
-     * Retrieves the list of all the nodes who responded to the initial discovery message
-     * @return the list of direct neighbours
-     */
 
     /**
      * Encapsulates the discovery packet in a Message Type
